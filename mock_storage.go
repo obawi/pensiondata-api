b8bac145c0b1a59f3146132cc2a14fc33f33b8dc
obawi <obawi@outlook.com>
2020-07-01 20:54:45 +0200

    Add comments for MockStore

--- mock_storage.go
+++ mock_storage.go
@@ -1,5 +1,6 @@
 package main
 
+// MockStore used for tests
 type MockStore struct {
 	ListFundsFn      func() ([]Fund, error)
 	GetFundByIsinFn  func(string) (Fund, error)
@@ -9,24 +10,32 @@ type MockStore struct {
 	CreateQuoteFn    func(string, ScraperCreateQuote) (Quote, error)
 }
 
+// ListFunds mock
 func (s MockStore) ListFunds() ([]Fund, error) {
 	return s.ListFundsFn()
 }
 
+// GetFundByIsin mock
 func (s MockStore) GetFundByIsin(isin string) (Fund, error) {
 	return s.GetFundByIsinFn(isin)
 }
 
+// ListQuotes mock
 func (s MockStore) ListQuotes(isin string) ([]Quote, error) {
 	return s.ListQuotesFn(isin)
 }
+
+// GetQuoteByDate mock
 func (s MockStore) GetQuoteByDate(isin, date string) (Quote, error) {
 	return s.GetQuoteByDateFn(isin, date)
 }
+
+// GetLatestQuote mock
 func (s MockStore) GetLatestQuote(isin string) (Quote, error) {
 	return s.GetLatestQuoteFn(isin)
 }
 
+// CreateQuote mock
 func (s MockStore) CreateQuote(isin string, scraperCreateQuote ScraperCreateQuote) (Quote, error) {
 	return s.CreateQuoteFn(isin, scraperCreateQuote)
 }
